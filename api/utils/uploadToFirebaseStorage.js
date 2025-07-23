25bb5399c0dd015f44e9b7639678d70e1623ac8f
narendra <narendra@gratefulness.me>
2025-07-23 22:21:48 +0530

    profile image name issue fix

--- api/utils/uploadToFirebaseStorage.js
+++ api/utils/uploadToFirebaseStorage.js
@@ -10,7 +10,7 @@ async function uploadFileToStorage(file) {
   const tempFilePath = path.join(os.tmpdir(), file.originalname);
   fs.writeFileSync(tempFilePath, file.buffer);
 
-  const storageFileName = `profilePhotos/${Date.now()}_${file.originalname}`;
+  const storageFileName = `profilePhotos/${file.originalname}`;
   const uploadOptions = {
     destination: storageFileName,
     metadata: {
@@ -25,9 +25,11 @@ async function uploadFileToStorage(file) {
 
   fs.unlinkSync(tempFilePath); // Clean up temp file
 
-  const downloadURL = `https://firebasestorage.googleapis.com/v0/b/${bucket.name}/o/${encodeURIComponent(
-    storageFileName
-  )}?alt=media&token=${uploadOptions.metadata.metadata.firebaseStorageDownloadTokens}`;
+  const downloadURL = `https://firebasestorage.googleapis.com/v0/b/${
+    bucket.name
+  }/o/${encodeURIComponent(storageFileName)}?alt=media&token=${
+    uploadOptions.metadata.metadata.firebaseStorageDownloadTokens
+  }`;
 
   return downloadURL;
 }
