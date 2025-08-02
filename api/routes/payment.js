b0e2c8659a31063dd47682ac17fb6af7bcc3d0f9
narendra <narendra@gratefulness.me>
2025-08-02 16:46:47 +0530

    plans id added

--- api/routes/payment.js
+++ api/routes/payment.js
@@ -18,14 +18,38 @@ router.get("/pro-plans", verifyJWT, async (req, res) => {
 
     const plans = isPremiumExam
       ? [
-          { durationInMonths: 12, price: 999 },
-          { durationInMonths: 6, price: 699 },
-          { durationInMonths: 1, price: 199 },
+          {
+            id: "34eaf2db-4e0e-45d0-b0d0-2d5a2c3e7498",
+            durationInMonths: 12,
+            price: 999,
+          },
+          {
+            id: "176b3e22-d819-4015-82ab-f875c17cb4f2",
+            durationInMonths: 6,
+            price: 699,
+          },
+          {
+            id: "6f08e545-8ad3-4df8-9176-8b9f26365df7",
+            durationInMonths: 1,
+            price: 199,
+          },
         ]
       : [
-          { durationInMonths: 12, price: 699 },
-          { durationInMonths: 6, price: 499 },
-          { durationInMonths: 1, price: 149 },
+          {
+            id: "f4d1c30d-5b72-4ec1-95a9-efb0e9279a34",
+            durationInMonths: 12,
+            price: 699,
+          },
+          {
+            id: "9e64c598-29d1-4e62-932d-7a535fd0f174",
+            durationInMonths: 6,
+            price: 499,
+          },
+          {
+            id: "a49d0b95-b33d-49ea-865c-41eb09cd1f49",
+            durationInMonths: 1,
+            price: 149,
+          },
         ];
 
     res.json({
