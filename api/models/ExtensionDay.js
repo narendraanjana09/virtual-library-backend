a2ec1ede73680bff7c219784bdf3e32a7a601635
narendra <narendra@gratefulness.me>
2025-10-17 20:40:41 +0530

    extension day error fix

--- api/models/ExtensionDay.js
+++ api/models/ExtensionDay.js
@@ -1,6 +1,21 @@
 // models/ExtensionData.js
 const mongoose = require("mongoose");
 
+const UserSchema = new mongoose.Schema(
+  {
+    userId: { type: String, required: true },
+    name: String,
+    imgUrl: String,
+    firstSeen: String,
+    lastSeen: String,
+    present: Boolean,
+    lastPresentAt: Number,
+    totalSeconds: { type: Number, default: 0 },
+    joinCount: { type: Number, default: 0 },
+  },
+  { _id: false }
+);
+
 const ExtensionDaySchema = new mongoose.Schema(
   {
     date: { type: String, required: true, index: true }, // "YYYY-MM-DD"
